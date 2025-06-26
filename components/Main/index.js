ad51b12bbd49a7686673e274bd1859a5c7cb02f9
Priyanka-saw <priyankasaw51@gmail.com>
2025-06-26 21:16:33 +0530

    updated

--- components/Main/index.js
+++ components/Main/index.js
@@ -7,3 +7,25 @@ themeToggler.addEventListener("click", () => {
   // Toggle icon highlight
   lightIcon.classList.toggle("active");
 });
+
+// 
+  window.addEventListener("scroll", revealOnScroll);
+
+  function revealOnScroll() {
+    const scrollElements = document.querySelectorAll(".scroll");
+
+    scrollElements.forEach((el) => {
+      const windowHeight = window.innerHeight;
+      const elementTop = el.getBoundingClientRect().top;
+      const revealPoint = 150;
+
+      if (elementTop < windowHeight - revealPoint) {
+        el.classList.add("active");
+      }
+    });
+  }
+
+  // Call once on load too
+  revealOnScroll();
+
+
