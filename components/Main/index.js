e222b76baccf69f110fc89360e90b25e2996d904
Priyanka-saw <priyankasaw51@gmail.com>
2025-06-29 21:28:31 +0530

    initial updates

--- components/Main/index.js
+++ components/Main/index.js
@@ -42,3 +42,31 @@ document.getElementById('projects-link')
     document.querySelector('#projects')
       .scrollIntoView({ behavior: 'smooth' });
   });
+
+ document.getElementById('contact-link').addEventListener('click', e => {
+  e.preventDefault();
+  document.querySelector('#contact').scrollIntoView({behavior: 'smooth'})
+ }) 
+ 
+// response through contacts
+  const form = document.querySelector(".contact-form");
+
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const response = await fetch(form.action, {
+      method: form.method,
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      alert("Message sent successfully!");
+      form.reset();
+    } else {
+      alert("Oops! Something went wrong.");
+    }
+  });
