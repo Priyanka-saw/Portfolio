1d8cbf491e9ceb5aca8532d4ea7e33e5dbb0a9d8
Priyanka-saw <priyankasaw51@gmail.com>
2025-07-01 19:46:50 +0530

    initial updates

--- components/Main/index.js
+++ components/Main/index.js
@@ -9,24 +9,39 @@ themeToggler.addEventListener("click", () => {
 });
 
 // 
-  window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("scroll", revealOnScroll);
 
-  function revealOnScroll() {
-    const scrollElements = document.querySelectorAll(".scroll");
+function revealOnScroll() {
+  const scrollElements = document.querySelectorAll(".scroll");
 
-    scrollElements.forEach((el) => {
-      const windowHeight = window.innerHeight;
-      const elementTop = el.getBoundingClientRect().top;
-      const revealPoint = 150;
+  scrollElements.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 150;
 
-      if (elementTop < windowHeight - revealPoint) {
-        el.classList.add("active");
-      }
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+// Call once on load too
+revealOnScroll();
+
+// navbar
+
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('navLinks');
+  const links = document.querySelectorAll('#navLinks a');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      navLinks.classList.remove('show');
     });
-  }
-
-  // Call once on load too
-  revealOnScroll();
+  });
 
 // scroll of direct sections
 document.getElementById('about-link')
@@ -43,30 +58,31 @@ document.getElementById('projects-link')
       .scrollIntoView({ behavior: 'smooth' });
   });
 
- document.getElementById('contact-link').addEventListener('click', e => {
+document.getElementById('contact-link').addEventListener('click', e => {
   e.preventDefault();
-  document.querySelector('#contact').scrollIntoView({behavior: 'smooth'})
- }) 
- 
+  document.querySelector('#contact').scrollIntoView({ behavior: 'smooth' })
+})
+
+
 // response through contacts
-  const form = document.querySelector(".contact-form");
+const form = document.querySelector(".contact-form");
 
-  form.addEventListener("submit", async function (e) {
-    e.preventDefault();
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
 
-    const formData = new FormData(form);
-    const response = await fetch(form.action, {
-      method: form.method,
-      body: formData,
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
-
-    if (response.ok) {
-      alert("Message sent successfully!");
-      form.reset();
-    } else {
-      alert("Oops! Something went wrong.");
+  const formData = new FormData(form);
+  const response = await fetch(form.action, {
+    method: form.method,
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
     }
   });
+
+  if (response.ok) {
+    alert("Message sent successfully!");
+    form.reset();
+  } else {
+    alert("Oops! Something went wrong.");
+  }
+});
